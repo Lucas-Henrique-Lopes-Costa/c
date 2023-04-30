a7bbd0ac89f5fc9655ff13ea42eb087b720f23f1
Lucas Henrique Lopes Costa <60316602+Lucas-Henrique-Lopes-Costa@users.noreply.github.com>
2023-04-30 14:17:10 -0300

    Update 19 - Conversão de Milissegundos.cpp

--- dreed/04-29 - Arquivos/19 - Conversão de Milissegundos.cpp
+++ dreed/04-29 - Arquivos/19 - Conversão de Milissegundos.cpp
@@ -12,14 +12,14 @@ int main()
   arquivoIn >> milissegundos;
 
   int horas = milissegundos / (1000 * 60 * 60);     // 2
-  cout << milissegundos / (1000 * 60 * 60) << endl; // 1546899,99984
+  // cout << milissegundos / (1000 * 60 * 60) << endl; // 1546899,99984
 
   int minutos = (milissegundos % (1000 * 60 * 60) / (1000 * 60));
-  cout << milissegundos % (1000 * 60 * 60) << endl;                 // 1546899,99984
-  cout << (milissegundos % (1000 * 60 * 60) / (1000 * 60)) << endl; // 25
+  // cout << milissegundos % (1000 * 60 * 60) << endl;                 // 1546899,99984
+  // cout << (milissegundos % (1000 * 60 * 60) / (1000 * 60)) << endl; // 25
 
   float segundos = (milissegundos % (1000 * 60) / 1000.0);
-  cout << milissegundos % (1000 * 60) << endl; // 46900
+  // cout << milissegundos % (1000 * 60) << endl; // 46900
 
   /*
     para converter milissegundos para horas, o programa divide o tempo em milissegundos por 1000 (para obter o tempo em segundos), em seguida, divide o tempo em segundos por 60 * 60 (para obter o tempo em horas).
