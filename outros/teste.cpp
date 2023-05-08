bfd68a0dd27cd5e50fb2d7469ef3018438e3c13d
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2023-05-08 17:48:11 -0300

    erro

--- outros/teste.cpp
+++ outros/teste.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 int main()
 {
-  int valor1 = 20, valor2 = 10, valor3 = 5;
-  if (valor1 and valor2 and (valor3 == 5))
-    cout << valor3 << endl;
-  else
-    cout << valor2 + valor1 << endl;
-  return 0;
+  int x = 0, y = 5;
+
+  while (x < 4 or y > 5)
+  {
+    x++;
+    y++;
+    y = y - x;
+  }
+
+  cout << x + y << endl;
 }
