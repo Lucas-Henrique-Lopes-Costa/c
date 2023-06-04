7682687a13739f16c9fb10840926ce3c3386cdd9
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2023-06-04 14:44:17 -0300

    62

---
+++ aulas/dreed/06-03 - Modularizacao/5 - doisMariores.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+void Max2(float numeros[], int tamanho, float &A, float &B)
+{
+  for (int i = 0; i < tamanho; i++)
+  {
+    if (numeros[i] > A)
+    {
+      B = A;
+      A = numeros[i];
+    }
+    else if (B < numeros[i])
+    {
+      B = numeros[i];
+    }
+  }
+}
+
+int main()
+{
+  /*
+    Escreva um subprograma chamado Max2. Esse subprograma deve receber um vetor de números reais, um inteiro contendo o tamanho do vetor e mais dois parâmetros reais A e B, passados por referência. Max2 não retorna valor (é uma “função void”). Este subprograma deve encontrar os dois maiores elementos do vetor e colocar o maior deles em A e o segundo maior em B.
+
+    A função principal deve passar o vetor e seu tamanho, receber os dois maiores e escrevê-los. O tamanho do vetor deve ser lido no início do programa.
+
+    Entradas:
+    O tamanho N do vetor;
+    Os N elementos do vetor, tipo real.
+
+    Saídas:
+    O maior elemento (A).
+    O segundo maior elemento (B).
+
+    Exemplo de entrada:
+    18
+    3 0 1 2 17 5 6 7 8 9 10 11 15 16 12 13 14 4
+
+    Exemplo de saída:
+    17 16
+  */
+
+  int n;
+  cin >> n;
+
+  if (n == 0)
+  {
+    cout << "0 0" << endl;
+    return 0;
+  }
+
+  float numeros[n];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> numeros[i];
+  }
+
+  float A = numeros[0];
+  float B = numeros[1];
+
+  if (A < B)
+  {
+    A = B;
+    B = A;
+  }
+
+  Max2(numeros, n, A, B);
+  cout << A << " " << B << endl;
+
+  return 0;
+}
