504e780d61846d59df2b5a6ef7d3cbaf1400c041
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2023-06-26 22:31:01 -0300

    100%

---
+++ aulas/dreed/06-26 - Ponteiro/1-pontoDentro.cpp
@@ -0,0 +1,58 @@
+/*
+Faça um programa que crie um registro Carro com os campos: marca, ano, distância percorrida na última viagem (em quilômetros) e consumo de combustível na última viagem (em litros). Seu programa deverá alocar dinamicamente uma variável do tipo Carro e ler, a partir de um arquivo texto chamado "entrada.txt", as informações de um carro em particular. Em seguida, faça um subprograma que receba como entrada as informações lidas e retorne à função principal quantos quilômetros por litro o carro em questão realizou em sua última viagem. As informações da marca, ano do carro, consumo de combustível na última viagem (em litros) e o resultado obtido pela função, devem ser gravados em um outro arquivo chamado "saida.txt". Assuma que o carro sempre terá consumido alguma quantidade de combustível.
+
+Entradas (conteúdo do arquivo):
+Cadeia de caracteres representando a marca de um carro (sem espaços).
+Inteiro indicando o ano de fabricação do carro.
+Ponto flutuante indicando a distância percorrida pelo carro em sua última viagem.
+Ponto flutuante indicando o consumo realizado pelo carro em sua última viagem.
+
+Saída (conteúdo do arquivo):
+Cadeia de caracteres representando a marca de um carro (sem espaços).
+Inteiro indicando o ano de fabricação do carro.
+Ponto flutuante indicando o consumo realizado pelo carro em sua última viagem.
+Ponto flutuante indicando quantos quilômetros por litro o carro realizou em sua última viagem.
+
+Exemplo de Entrada (conteúdo do arquivo):
+palio
+2008
+62.5
+5.3
+
+Exemplo de Saída (conteúdo do arquivo):
+palio
+2008
+5.3
+11.79
+*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Carros
+{
+  string marca;
+  int ano;
+  float distancia;
+  float consumo;
+};
+
+void informacoes(Carros carro[], ofstream &arquivoOut)
+{
+  arquivoOut << carro[0].consumo << endl;
+  arquivoOut << carro[0].distancia / carro[0].consumo << endl;
+}
+
+int main()
+{
+  ifstream arquivoIn("entrada.txt");
+  ofstream arquivoOut("saida.txt");
+  
+  Carros *carro = new Carros[1];
+  arquivoIn >> carro[0].marca >> carro[0].ano >> carro[0].distancia >> carro[0].consumo;
+
+  arquivoOut << carro[0].marca << endl << carro[0].ano << endl;
+  informacoes(carro, arquivoOut);
+
+  return 0;
+}
